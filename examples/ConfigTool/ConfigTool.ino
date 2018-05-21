992a32fa670ce40d04858c85b43b0237d633fd3d
Tim van den Essen <tim3345@live.nl>
2018-05-21 16:22:00 +0200

    add examples

---
+++ examples/ConfigTool/ConfigTool.ino
@@ -0,0 +1,43 @@
+#include "ConfigTool.h"
+
+struct Config {
+	String text = "This is my default text.";
+	int number = 100;
+};
+
+Config config;
+ConfigTool configTool;
+
+void setup() {
+	Serial.begin(115200);
+	Serial.println();
+
+	configTool.addVariable("Test", &config.text);
+	configTool.addVariable("Some Number", &config.number);
+	
+	configTool.load();
+
+	Serial.println(config.text);
+	Serial.println(config.number);
+
+	config.name = "I can just assign new values like this.";
+	config.number = 200;
+
+	Serial.println(config.text);
+	Serial.println(config.number);
+
+	configTool.save();
+	//Values are saved now and will be put back on reset with .load
+
+	configTool.reset();
+	//Reset the tool and assign the default values.
+
+	Serial.println(config.text);
+	Serial.println(config.number);
+
+	configTool.save();
+}
+
+void loop() {
+
+}
